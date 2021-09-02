e276da11a64724564268f9434abb3c1bb2147f13
Vitalii Bairak <vitalii_b88@yahoo.com>
2021-09-03 00:59:57 +0300

    Added i18next libraries

---
+++ src/i18n/en.js
@@ -0,0 +1,7 @@
+export default {
+  translation: {
+    already_exists: 'RSS already exists',
+    must_be_url: 'must be valid URL',
+    sucsses: 'RSS loaded successfully',
+  },
+};
---
+++ src/i18n/ru.js
@@ -0,0 +1,7 @@
+export default {
+  translation: {
+    already_exists: 'RSS уже существует',
+    must_be_url: 'Ссылка должна быть валидным URL',
+    sucsses: 'RSS успешно загружен',
+  },
+};
